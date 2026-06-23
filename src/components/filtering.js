cb70f2a5c879becaf7bfdd218bc3e1aee5d289cc
Evgeniy Sapozhnikov <engineer.esa@yandex.ru>
2026-06-23 22:22:14 +0300

    первая ревизия

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,42 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes) // Получаем ключи из объекта
+    .forEach((elementName) => {
+      // Перебираем по именам
+      elements[elementName].append(
+        // в каждый элемент добавляем опции
+        ...Object.values(indexes[elementName]) // формируем массив имён, значений опций
+          .map((name) => {
+            // используйте name как значение и текстовое содержимое
+            const newTag = document.createElement("option");
+            newTag.value = name;
+            newTag.textContent = name;
+            return newTag;
+          }),
+      );
+    });
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (action && action.name === "clear") {
+      const fieldName = action.getAttribute("data-field");
+      const parentContainer = action.parentElement;
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+      const inputCell = parentContainer.querySelector("input");
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+      if (inputCell) {
+        inputCell.value = "";
+      }
+
+      if (fieldName) {
+        state[fieldName] = "";
+      }
     }
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    return data.filter((row) => compare(row, state));
+  };
 }
